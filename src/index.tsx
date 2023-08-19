474eb4772c0c51b2384a95135bb4b06839b5ce45
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-08-19 22:40:02 +0530

    Data Context  Provider Added To Index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import AuthContextProvider from './Contexts/AuthContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./Contexts/AuthContext";
+import DataContextProvider from "./Contexts/DataContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthContextProvider>
-     <App />
-     </AuthContextProvider>
+      <AuthContextProvider>
+        <DataContextProvider>
+          <App />
+        </DataContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
