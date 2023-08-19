f53ca90d563a1d43fc63ec3c86b8ceb80b988dd1
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-08-19 22:40:48 +0530

    Fix: Added Placeholder Div for header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,19 +5,23 @@ import { BsBell, BsPersonCircle, BsSearch } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <header className="top-bar">
-      <span className="logo">TaskLeafs</span>
-      <button title="search">
-        <BsSearch size={"1.5rem"} />
-      </button>
-      <button title="Notifications">
-        <BsBell size={"1.5rem"} />
-      </button>
-      <button title="person">
-        <BsPersonCircle size={"1.5rem"} />
-      </button>
-      <ThemeButton />
-    </header>
+    <>
+      <div className="header-placeholder"></div>
+
+      <header className="top-bar">
+        <span className="logo">TaskLeafs</span>
+        <button title="search">
+          <BsSearch size={"1.5rem"} />
+        </button>
+        <button title="Notifications">
+          <BsBell size={"1.5rem"} />
+        </button>
+        <button title="person">
+          <BsPersonCircle size={"1.5rem"} />
+        </button>
+        <ThemeButton />
+      </header>
+    </>
   );
 };
 
