b14751bd76ab2aa94bb7ef7977ac1154d8b8f294
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-08-20 23:17:05 +0530

    Restructured Dashboard

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import { DataContext } from "../../Contexts/DataContext";
 import { Link } from "react-router-dom";
 import { type } from "os";
 import axios from "axios";
+import { BsPlusLg } from "react-icons/bs";
 
 const Dashboard: React.FC = () => {
   const {
@@ -36,28 +37,31 @@ const Dashboard: React.FC = () => {
     <>
       <Header />
       <div className="dashboard">
-        <div className="workplaces">
-          <h2 className="workplaces-heading f-ll">Your Workplaces</h2>
-          <div className="workplace-list">
-            {workplaces?.map((workplace: any) => (
-              <Link to="/" className="workplace-link f-ssm" key={workplace._id}>
-                <div className="workplace-name">{workplace.name}</div>
-              </Link>
-            ))}
-            <div className="workplace-link f-ssm" key={"reached-end"}>
-              <div className="workplace-name">
-                You have reached the end, but you can always create more
-              </div>
-            </div>
+        <button className="create-workplace-button f-ssm">
+          {<BsPlusLg size={"1.5rem"} />}
+          <span>Create Workplace</span>
+        </button>
+        {workplaces?.map((workplace: any) => (
+          <Link
+            to={`/wp/${workplace._id}`}
+            className="workplace-link f-ssm"
+            key={workplace._id}
+          >
+            <div className="workplace-name">{workplace.name}</div>
+          </Link>
+        ))}
+        <div className="workplace-link f-ssm" key={"reached-end"}>
+          <div className="workplace-name">
+            You have reached the end, but you can always create more
           </div>
         </div>
-        <div className="create-workplace">
+        {/* <div className="create-workplace">
           <form onSubmit={createWorkplace}>
             <input type="text" placeholder="Workplace Name" />
-            <input type="text" placeholder="Workplace Description" />
+            <textarea placeholder="Workplace Description" />
             <button type="submit">Create Workplace</button>
           </form>
-        </div>
+        </div> */}
       </div>
     </>
   );
