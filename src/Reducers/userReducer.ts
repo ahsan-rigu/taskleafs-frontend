a579c43494726418f3f134469bf70b231abaa9b7
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-08-26 23:53:55 +0530

    misc

--- src/Reducers/userReducer.ts
+++ src/Reducers/userReducer.ts
@@ -11,8 +11,46 @@ export const userReducer = (state: any, action: any) => {
             ...state,
             workplaces: [...state.workplaces, action.payload],
         };
+        case "MOVE_LEAF": {
+            const { workplaceid,  branch} = action.payload;
+            const newWorkplaces = state.workplaces.map((workplace: any) => {
+                if (workplace._id === workplaceid) {
+                    const newBranches = workplace.branches.map((b: any) => {
+                        if (b._id === branch._id) {
+                            return branch;
+                        }
+                        return b;
+                    });
+                    return { ...workplace, branches: newBranches };      
+                }
+                return workplace;
+            }
+            );
+            return { ...state, workplaces: newWorkplaces };
+        }
+        case "MOVE_TASK":{ 
+            const { workplaceid, branch } = action.payload;
+            const newWorkplaces = state.workplaces.map((workplace: any) => {
+                if (workplace._id === workplaceid) {
+                    const newBranches = workplace.branches.map((b: any) => {
+                        if (b._id === branch._id) {
+                            return branch;
+                        }
+                        return b;
+                    });
+                    return { ...workplace, branches: newBranches };
+                }
+                return workplace;
+            }
+            );
+            return { ...state, workplaces: newWorkplaces };
+        }
+        
+
         default:
         return state;
+
+
     }
 
     };
